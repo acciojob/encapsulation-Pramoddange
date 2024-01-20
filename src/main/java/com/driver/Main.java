bde686115c97430456dd4fa65526a3e32c9814fc
Pramod Dange <pramodsdange1@gmail.com>
2024-01-21 02:32:02 +0530

    encapsulation is compeleted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+
+
+        // Task 4: Try setting a value to 'name' directly and print it
+        // Error: You will get a compilation error since 'name' is private
+        // obj.name = "John";  // Uncommenting this line will result in an error
+        // System.out.println(obj.name);  // Uncommenting this line will result in an error
+
+        // Task 6: Set a value to 'name' using the setter function and access it using the getter function
+        obj.setName("John");
+        String retrievedName = obj.getName();
+        System.out.println("Name retrieved using getter: " + retrievedName);
+    }
 }
+
+  
